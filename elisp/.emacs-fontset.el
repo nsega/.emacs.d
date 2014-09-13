0b6fcd766475bc65600020f7bb774e77b2403733
Naoki Sega <nsegaster@gmail.com>
2014-09-13 15:06:00 +0900

    first commit

---
+++ elisp/.emacs-fontset.el
@@ -0,0 +1,6 @@
+(setq default-frame-alist
+  (append
+  '((font . "fontset-14") ;; デフォルトフォントセット
+  ;(width . 80) (height . 40) ;; ウィンドウサイズ
+default-frame-alist)))
+
---
+++ elpa/js2-mode-20090814/js2-mode-autoloads.el
@@ -0,0 +1,30 @@
+;;; js2-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (js2-mode) "js2-mode" "js2-mode.el" (20963 29283
+;;;;;;  0 0))
+;;; Generated autoloads from js2-mode.el
+ (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(autoload 'js2-mode "js2-mode" "\
+Major mode for editing JavaScript code.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("js2-mode-pkg.el") (20963 29283 440326
+;;;;;;  0))
+
+;;;***
+
+(provide 'js2-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; js2-mode-autoloads.el ends here
