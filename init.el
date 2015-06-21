d4e2887621981b6fa13c452952e66d572d674bef
Naoki Sega <nsegaster@gmail.com>
2015-06-22 03:01:35 +0900

    modify migemo configuration

--- init.el
+++ init.el
@@ -7,6 +7,10 @@
 	)
        load-path))
 
+(setq exec-path (cons "/usr/local/bin" exec-path))
+(setenv "PATH"
+    (concat '"/usr/local/bin:" (getenv "PATH")))
+
 ;;　起動ディレクトリをホームディレクトリに変更する(for Mervelicks)
 (setq default-directory "~/") 
 (setq command-line-default-directory "~/")
@@ -158,6 +162,12 @@
 (require 'migemo)
 (setq migemo-command "cmigemo")
 (setq migemo-options '("-q" "--emacs"))
+;; Set your installed path
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(migemo-init)
 
 ;; auto-complete
 (require 'auto-complete)
