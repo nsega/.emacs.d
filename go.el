510d25dfddf1ad87529ff2f9536b08ef599516bc
Naoki Sega <nsegaster@gmail.com>
2015-10-12 00:39:05 +0900

    added go.el

---
+++ go.el
@@ -0,0 +1,13 @@
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
+
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-f") 'gofmt)))
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook '(lambda ()
+  (local-set-key (kbd "C-c C-k") 'godoc)))
+
